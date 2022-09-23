85187d2cdf672ce129fa9e821322f7db7941ab5f
aguranchik <a.guranchik@21vek.by>
2022-09-23 16:23:39 +0300

    exchange_rate

--- cURL/exchange_rate.sh
+++ cURL/exchange_rate.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 str=$(curl -s https://myfin.by/bank/kursy_valjut_nbrb)
+
 ex_rate(){
-    echo $str |  grep 'a href="/bank/kursy_valjut_nbrb/' | sed 's:<a href="/bank/kursy_valjut_nbrb/:\n(&):g' | sed '1d' | sed 's:data-key="[0-9]*"><td>:(&)\n:g' | sed '$d' | sed '/^$/d' | grep -i $1 | awk -F"</td><td>" '{print $2}'
+  echo $str |  grep 'a href="/bank/kursy_valjut_nbrb/' | sed 's:<a href="/bank/kursy_valjut_nbrb/:\n(&):g' | sed '1d' | sed 's:data-key="[0-9]*"><td>:(&)\n:g' | sed '$d' | sed '/^$/d' | grep -i $1 | awk -F"</td><td>" '{print $2}'
 }
 
 menu(){
   select currency
   do
-    # if [[ $currency == "Exit" ]]
-    # then
-    #   exit_state=1
-    #   break
-    # elif [[ $@ == *"$currency"* ]]
-    # then
-    #   echo "Exchange rate $currency: " $(ex_rate $currency)
-    #   break       
-    # else
-    #   echo "Input ERROR"
-    #   break
-    # fi
     case $currency in
       Exit)
         exit_state=1
